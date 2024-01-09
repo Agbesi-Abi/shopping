9803036580d03370aeacd5e26d0604746e8e7e71
Agbesi Abigail <agbesia55@gmail.com>
2024-01-09 12:15:50 +0000

    initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ritamsantra2003@gmail.com'
+EMAIL_HOST_USER = 'agbesia55@gmail.com'
 EMAIL_HOST_PASSWORD = 'gpzzfdgucfdkycyc'
 
 
